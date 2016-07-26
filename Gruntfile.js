90953feb4888d32f2e46d4f307a1a0baaea82a7c
lijuanxia <651848442@qq.com>
2016-07-26 10:01:28 +0800

    gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,7 +13,7 @@ module.exports = function(grunt) {
 	// Configurable paths
 	var config = {
 		activity: 'activity',
-		actName: 'weixinShake',
+		actName: 'semiannualReport',
 		dist: 'dist'
 	};
 
