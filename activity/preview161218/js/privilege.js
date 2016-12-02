5f65728975a5eae81f9e90c715b73d18724c965b
卢俊伟 <lujunwei@lujunweideMacBook-Pro.local>
2016-12-02 17:36:45 +0800

    增加签到未中奖状态

--- activity/preview161218/js/privilege.js
+++ activity/preview161218/js/privilege.js
@@ -26,22 +26,35 @@
 		});
 	});
 
-	//领取加息劵
-	pageContentEl.on("click", ".btn-getOnePercent", function() {
-		// TODO: 判断是否登录
-		// 登录之后
-		Util.alertJiaxi("1", function() { //点击关闭按钮
+	//显示签到
+	pageContentEl.on("click", ".btn-sign", function() {
+		Util.alertCommon({
+			type: 1,
+			content: "<p style='font-size:0.64rem;line-height:1.28rem;'>活动期间内，在APP进行签到并成功分享，即有机会获得<font style='color:#ff7630;'>0.1%</font>加息特权。</p>",
+			btn: {
+				name: "马上签到",
+				callback: function() {
+					// TODO: 跳转至手机签到链接
+					window.location.href = ""
+				}
+			}
+		});
+	});
 
-			onePercentTaskEl.find(".task-status").addClass("done");
-			// 设置总的加息特权 百分比根据实际变化
-			interestTextEl.text("1");
-		})
+	//领取加息劵
+	pageContentEl.on("click", ".btn-sign-nothing", function() {
+		Util.alertCommon({
+			type: 3,
+			content: '<p>噢哦，未抽中加息特权～</p><p>明日再接再厉，签到赢加息特权</p>'
+		});
 	});
 
 	$(".popup-close, .masker").on("click", function() {
 		$(".dialog").hide();
 	})
-	var statusSign = 2; //手机签到状态 0当天未签到	1当天未获加息	2当天获加息
+
+	// 通过以下状态来控制页面
+	var statusSign = 1; //手机签到状态 0当天未签到	1当天未获加息	2当天获加息
 	var statusOnePercent = 0; //是否获取1%加息特权 0未获取	1已获取	
 	var statusInvest = 0; //是否获取投资加息 0未获取	 1已获取	
 	var statusInvite = 0; //是否获取邀请加息 0未获取	 1已获取	
@@ -67,9 +80,12 @@
 	var uiSetStatus = function() { //设置页面状态
 
 		switch (statusSign) { //签到
-			case 2:
-			case 1:
+			case 2: //获得加息
 				signTaskEl.find(".task-status").addClass("done");
+				break;
+			case 1: //未获得加息
+				signTaskEl.find(".task-done, .btn-sign").hide();
+				signTaskEl.find(".btn-sign-nothing, .not-win").css("display", "inline-block");
 				break;
 		}
 		switch (statusOnePercent) { //1%
