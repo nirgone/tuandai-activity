327c43978a7a8c77dc47ca2733150776936c1011
卢俊伟 <lujunwei@lujunweideMacBook-Pro.local>
2016-12-07 10:41:35 +0800

    爱心特权1207修改

--- activity/preview161218/js/privilege.js
+++ activity/preview161218/js/privilege.js
@@ -3,47 +3,46 @@
 	var pageContentEl = $(".page-content");
 	var signTaskEl = pageContentEl.find(".get-interest-sign"); //签到任务dom
 	var onePercentTaskEl = pageContentEl.find(".get-interest-onepercent"); //1%加息任务dom
+	var tuanbiTaskEl = pageContentEl.find(".get-interest-tuanbi"); //1%加息任务dom
 	var investTaskEl = pageContentEl.find(".get-interest-invest"); //投资任务dom
 	var inviteTaskEl = pageContentEl.find(".get-interest-invite"); //邀请任务dom
 	var interestTextEl = pageContentEl.find(".interest-ticket-wrapper font"); //加息文本dom
 
-	var isApp = true; //是否为app端打开
+	var isApp = false; //是否为app端打开
 	var isLogined; //是否已登录
 	var option;
 	//获取登录状态
 	if (isApp) {
 		option = Util.getParam('option');
 		isLogined = Util.checkLogin(option);
-		// console.info("juan------", isLogined);
 	} else {
 		//获取触屏版登录状态
 	}
+
 	// 查看加息说明
 	pageContentEl.on("click", ".check-rule", function() {
 		$(".dialog").show();
 	});
 
-	//领取1%加息
-	pageContentEl.on("click", ".get-interest-onepercent", function() {
-		Util.alertJiaxi("1", function() {
-			onePercentTaskEl.find(".task-status").addClass("done");
-			// 设置总的加息特权 百分比根据实际变化
-			interestTextEl.text("1");
-		});
-
-
-	});
+	// 关闭弹窗
+	$(".popup-close, .masker").on("click", function() {
+		$(".dialog").hide();
+	})
 
 	//显示签到
 	pageContentEl.on("click", ".btn-sign", function() {
 		Util.alertCommon({
 			type: 1,
-			content: "<p style='font-size:0.64rem;line-height:1.28rem;'>活动期间内，在APP进行签到并成功分享，即有机会获得<font style='color:#ff7630;'>0.1%</font>加息特权。</p>",
+			content: "<p style='font-size:0.64rem;line-height:1.28rem;'>活动期间内，在APP进行签到并成功分享，即有机会获得<font style='color:#ff7630;'>0.05%</font>加息特权。</p>",
 			btn: {
 				name: "马上签到",
 				callback: function() {
-					// TODO: 跳转至手机签到链接
-					window.location.href = ""
+					if (!isApp) { //不是app
+						window.location.href = "https://m.tuandai.com/pages/downopenapp.aspx";
+					} else {
+						// TODO: 跳转至手机签到链接
+						window.location.href = ""
+					}
 				}
 			},
 			closeCallback: function() {
@@ -52,7 +51,14 @@
 		});
 	});
 
-	//领取加息劵
+	//签到加息抽奖
+	pageContentEl.on("click", ".btn-lottery", function() {
+
+		var isGetInterest = 1; // 0当天未获加息	1当天获加息
+		uiShowSign(isGetInterest)
+	});
+
+	//没获取加息劵
 	pageContentEl.on("click", ".btn-sign-nothing", function() {
 		Util.alertCommon({
 			type: 3,
@@ -63,41 +69,54 @@
 		});
 	});
 
-	$(".popup-close, .masker").on("click", function() {
-		$(".dialog").hide();
-	})
+	//领取1%加息
+	pageContentEl.on("click", ".get-interest-onepercent", function() {
+		Util.alertJiaxi("1", function() {
+			onePercentTaskEl.find(".task-status").addClass("done");
+			// 设置总的加息特权 百分比根据实际变化
+			interestTextEl.text("1");
+		});
+	});
+
+
 
 	// 通过以下状态来控制页面
 	var statusSign = 0; //手机签到状态 0当天未签到	1当天未获加息	2当天获加息
 	var statusOnePercent = 0; //是否获取1%加息特权 0未获取	1已获取	
+	var statusTuanbi = 1; //是否获取团币加息 0未获取 	1已获取	
 	var statusInvest = 0; //是否获取投资加息 0未获取	 1已获取	
 	var statusInvite = 0; //是否获取邀请加息 0未获取	 1已获取	
 
-	var isShowSignDialog = false; //是否显示签到中奖与否弹窗
-	var uiShowSign = function() { //是否显示签到中奖与否弹窗
-		if (isShowSignDialog) {
-			switch (statusSign) { //签到
-				case 2: //获得加息
-					Util.alertJiaxi("0.1", function() {
-
-					})
-					break;
-				case 1: //未获得加息
-					Util.alertCommon({
-						type: 3,
-						content: '<p>噢哦，未抽中加息特权～</p><p>明日再接再厉，签到赢加息特权</p>',
-						closeCallback: function() {
-
-						}
-					});
-					break;
-			}
+	var isShowLottery = false; //是否显示抽奖
+	var uiShowLottery = function() { //是否显示签到中奖与否弹窗
+		if (isShowLottery) {
+			signTaskEl.find(".task-done, .btn-sign").hide();
+			signTaskEl.find(".btn-lottery").css("display", "inline-block");
 		}
 
 	}
 
-	var uiSetStatus = function() { //设置页面状态
 
+	var uiShowSign = function(isGetInterest) { //是否签到中奖
+		switch (isGetInterest) { //是否签到中奖
+			case 1: //获得加息
+				Util.alertJiaxi("0.05", function() {
+					signTaskEl.find(".task-status").addClass("done");
+				})
+				break;
+			case 0: //未获得加息
+				Util.alertCommon({
+					type: 3,
+					content: '<p>噢哦，未抽中加息特权～</p><p>明日再接再厉，签到赢加息特权</p>',
+					closeCallback: function() {
+
+					}
+				});
+				break;
+		}
+	}
+
+	var uiSetStatus = function() { //设置页面状态
 		switch (statusSign) { //签到
 			case 2: //获得加息
 				signTaskEl.find(".task-status").addClass("done");
@@ -107,25 +126,34 @@
 				signTaskEl.find(".btn-sign-nothing, .not-win").css("display", "inline-block");
 				break;
 		}
+
 		switch (statusOnePercent) { //1%
 			case 1:
 				onePercentTaskEl.find(".task-status").addClass("done");
 				break;
 		}
+
+		switch (statusTuanbi) { //团币
+			case 1:
+				tuanbiTaskEl.find(".task-status").addClass("done");
+				break;
+		}
+
 		switch (statusInvest) { //投资加息
 			case 1:
 				investTaskEl.find(".task-status").addClass("done");
 				break;
 		}
+
 		switch (statusInvite) { //邀请加息
 			case 1:
 				inviteTaskEl.find(".task-status").addClass("done");
 				break;
 		}
 		// 设置总的加息特权 百分比根据实际变化
-		interestTextEl.text("1");
+		interestTextEl.text("X");
 	}
-	uiShowSign(); //是否显示签到中奖与否弹窗
+	uiShowLottery(); //是否显示签到中奖与否弹窗
 	uiSetStatus(); //设置页面状态
 
 	/**/
