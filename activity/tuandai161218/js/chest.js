f9740717e74f105ce2ab0f54da16c20954834e15
卢俊伟 <lujunwei@lujunweideMacBook-Pro.local>
2016-12-11 23:17:18 +0800

    爱心宝箱

--- activity/tuandai161218/js/chest.js
+++ activity/tuandai161218/js/chest.js
@@ -1,4 +1,21 @@
 (function() {
-    FastClick.attach(document.body);
-	    //do your thing.
+	FastClick.attach(document.body);
+	//do your thing.
+	var pageEl = $(".content");
+
+
+	// 控制列表滚动
+	function anit() {
+		var length = pageEl.find(".winner-item").length;
+		var speed = 0.5 * length;
+		if (length >= 6) {
+			pageEl.find(".wrap").addClass('anit');
+			pageEl.find(".anit").css({
+				"-webkit-animation-duration": speed + "s",
+				"animation-duration": speed + "s",
+			});
+		}
+	}
+	// 控制列表滚动
+	anit();
 })();
