0772134aeffd9bd4870bacd1625e66d7d934ef4f
团贷网 <tuandai@tuandaiwangdeiMac.local>
2016-12-11 15:29:08 +0800

    留言板，公共样式

---
+++ activity/tuandai161218/js/msgboard.js
@@ -0,0 +1,8 @@
+(function() {
+    FastClick.attach(document.body);
+	    //do your thing.
+	    $("#descri").on("click", ".icon-open-arrow", function(e) {
+	    	console.log(1324234);
+	    	$("#descri").html("团贷网的每一个网贷爱心日，感恩团粉的陪伴，支持和信任。在这个特别的日子，我们一同为小微企业助力，一同为彼此加油鼓励，朝着爱心和梦想前行。请在这里留下您宝贵的祝福，让我们共同见证每一个12.18网贷爱心日。");
+	    });
+})();
