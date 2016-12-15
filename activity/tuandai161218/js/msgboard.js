b6e7f5de4aca90fc40fa26f6ced9e0eacbfeb789
团贷网 <tuandai@tuandaiwangdeiMac.local>
2016-12-15 17:42:56 +0800

    为爱发声修改

--- activity/tuandai161218/js/msgboard.js
+++ activity/tuandai161218/js/msgboard.js
@@ -1,9 +1,13 @@
 (function() {
     FastClick.attach(document.body);
     //do your thing.
-    $("#descri").on("click", ".icon-open-arrow", function(e) {
-        console.log(1324234);
+    $("#descri, .icon-open-arrow").on("click", function(e) {
+        var _unfold = $("#descri").attr("data-unfold");
+        if(_unfold === "1" ) {
+            return;
+        }
         $("#descri").html("团贷网的每一个网贷爱心日，感恩团粉的陪伴，支持和信任。在这个特别的日子，我们一同为小微企业助力，一同为彼此加油鼓励，朝着爱心和梦想前行。请在这里留下您宝贵的祝福，让我们共同见证每一个12.18网贷爱心日。");
+        $("#descri").attr("data-unfold", 1);
     });
 
     //拉起输入碳层
