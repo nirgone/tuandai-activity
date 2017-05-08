ec72bb96394940bae44972d855cfaab0cfdf8503
lanyuejie <lanyj123@163.com>
2017-05-08 17:33:21 +0800

    降息调查问卷活动增加答案编号

--- activity/questionnaire20170428/js/questions.js
+++ activity/questionnaire20170428/js/questions.js
@@ -1,6 +1,8 @@
 (function() {
 	FastClick.attach(document.body);
 	var curQueIndex = 0;
+	// 答案编号
+	var seriArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 	// 调查问卷问题初始化
 	function questionInit(){
@@ -10,12 +12,14 @@
 			item.options.forEach(function(opt, i) {
 				var _id = '' + index + i;
 				var txt_input = '';
+				// 如果选项有输入框
 				if (opt.hasInput) {
-					txt_input = '<input type="text" />'
+					txt_input = '<input type="text" class="option-input" data-id="'+ _id +'"/>'
 				}
+				// 选项节点
 				options_li += '<li>'+
                                 '<div class="option-radio">'+
-                                    '<input id="'+ _id +'" type="radio" name="'+ index +'">'+
+                                    '<input id="'+ _id +'" type="radio" name="'+ index +'" value="'+ seriArr[i % 26] +'">'+
                                     '<label class="icon-radio" for="'+ _id +'"></label>'+
                                 '</div>'+
                                 '<label class="option-txt" for="'+ _id +'">'+ opt.text + txt_input +'</label>'+
@@ -36,13 +40,16 @@
 		_questions.hide();
 		_questions.eq(index).show();
 		switch(index) {
+			// 第一题
 			case 0:
 				$("#btn-pre").hide();
 				break;
+			// 最后一题
 			case _questions.length - 1:
 				$("#btn-next").hide();
 				$("#btn-submit").show();
 				break;
+			// 默认
 			default:
 				$("#btn-submit").hide();
 				$("#btn-next").show();
@@ -54,11 +61,18 @@
 	// 事件绑定
 	function bindEvent() {
 		$("#btn-next").on('click',function() {
-			var curQue = questionList[curQueIndex];
-			var curArr = $("input[name='" + curQue.id + "']:checked");
+			var curQue = questionList[curQueIndex]; //当前第几个问题
+			var curArr = $("input[name='" + curQue.id + "']:checked"); //当前问题第几个答案被选中
+			var curId = '' + curQueIndex + curQue.id;
+			var optionInputVal = ''; //当前选项的输入框值
 			if(curArr.length === 0){
 				Util.toast('你的答案不能为空');
 				return;
+			}
+			// 获取选项输入框的值
+			if(curQue.hasInput){
+				optionInputVal = $(".option-input[data-id='"+ curId +"']").val();
+				optionInputVal = optionInputVal ? optionInputVal.trim() : '';
 			}
 			curQueIndex += 1;
 			questionShow();
