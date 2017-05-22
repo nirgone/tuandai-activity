246b53e7bf548c1c5028fe766065e288d6c6039b
团贷网 <825928023@qq.com>
2017-05-22 11:00:37 +0800

    加入直播列表的js

--- activity/live/js/index.js
+++ activity/live/js/index.js
@@ -1,4 +1,33 @@
 (function() {
-	FastClick.attach(document.body);
-	Util.toast('嘘！主播休息中...');
+    FastClick.attach(document.body);
+    // Util.toast('嘘！主播休息中...');
+
+    $('body').on('click', '.r-row', function() {
+        if ($(this).hasClass('r-live')) {
+            location.href = './broadcastRoom.html';
+        } else {
+            util.toast('嘘！主播休息中...');
+        }
+    })
+
+    Util.setSessionStorage('USER_INFO', { "id": "100033", "name": "test1" });
+
+    // 获取im参数
+    Util.Ajax({
+        "url": "http://10.103.8.188:1021/live/get-im-params.html",
+        "data": {
+            "id": "100033",
+            "name": "test1"
+        },
+        "type": "get",
+        "dataType": "json",
+        cbOk: function(data, textStatus, jqXHR) {
+            console.log(data);
+            window.sessionStorage['LOGIN_INFO'] = JSON.stringify(data);
+        },
+        cbErr: function(e, xhr, type) {
+
+        }
+    })
+
 })();
